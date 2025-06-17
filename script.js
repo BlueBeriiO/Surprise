3be5f750d7ee29e3ecad4d76e9a4eaadf3afd702
BlueBerii <127707798+BlueBeriiO@users.noreply.github.com>
2025-06-17 19:42:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function checkScene1() {
   const code = document.getElementById('code').value.trim();
-  if (code === "25670805") {
+  if (code === "20240805") {
     window.location.href = "./room2.html";
   } else {
-    document.getElementById('msg').innerText = "❌ คำตอบผิด ลองนึกถึงวันคบกันสิ 💌";
+    document.getElementById('msg').innerText = "❌ คำตอบผิด ลองนึกถึงวันพิเศษของเรา 💌";
   }
 }
+
 function checkScene2() {
   const code = document.getElementById('code').value.trim().toLowerCase();
   if (code === "เธอ") {
